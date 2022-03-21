bce314959b76ff54c643960f76372c8e408d1be3
jecastrom <jcastro.de@outlook.com>
2022-03-21 15:03:53 +0100

    a

--- a.sql
+++ a.sql
@@ -1,12 +1,14 @@
 SELECT
-    concat((first_name), ' ', (last_name)) rental_customers,
-    email
+    c.customer_id,
+    concat((c.first_name), ' ', (c.last_name)) rental_customers,
+    c.email,
+    format(avg(p.amount), 2, 'de_DE') average_payment_made
 FROM
-    rental r
-    INNER JOIN customer USING (customer_id)
+    payment p
+    INNER JOIN customer c USING (customer_id)
 GROUP BY
-    2
-ORDER BY
     1
+ORDER BY
+    2
 LIMIT
     5;
